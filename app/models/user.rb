671fe25c0b309a42f9f983a07cc22f11fd7549ae
Zakaria Aziz <zaxstyle@hotmail.com>
2014-05-30 00:44:55 +0900

    layout design

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+    has_many :status
+
+    validates :first_name, :last_name, :profile_name, presence: true
+
+    def full_name
+    	first_name + " " + last_name
+    end
 end
